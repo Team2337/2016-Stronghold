be3bdfdff0a847290875b6b7f7dd0d1b4c5ac1ec
abc <abc@abc.com>
2016-01-30 14:31:08 -0500

    shooter arm things
    delete the old shooter arm code. Add new command 4 the base, short shot,
    long shot, scale. Made operator Controls button don't have the right
    numbers

---
+++ RobotProject2016/src/org/usfirst/frc2337/RobotProject2016/commands/shooterArm_armSetPointShortShot.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc2337.RobotProject2016.commands;
+
+import org.usfirst.frc2337.RobotProject2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class shooterArm_armSetPointShortShot extends Command {
+
+	int setpoint; 
+	
+	public shooterArm_armSetPointShortShot(int setpoint) {
+		requires(Robot.shooterArmPID);
+        this.setpoint = setpoint;		
+	}
+
+	
+	protected void initialize() {
+		Robot.shooterArmPID.armSetpoint(setpoint);
+	}
+	
+	
+	protected void execute() {
+	}
+
+
+	protected boolean isFinished() {
+		return (Robot.shooterArmPID.onTarget());
+	}
+
+
+	protected void end() {	
+	}
+
+	
+	protected void interrupted() {
+		this.end();	
+	}
+}
